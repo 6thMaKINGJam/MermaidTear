3a2329464fd54352e5e5d39401023a629a6eecf3
nebobyeoli <nebobyeoli@gmail.com>
2025-12-20 10:51:02 +0900

    Player 애니메이션 추가

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
     private int _playerDirH;
     private int _playerDirV;
     [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _animSpeed = 1;
 
     //private Rigidbody2D _rb;
+    private Animator _anim;
 
     private void Start()
     {
         //_rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -26,6 +29,12 @@ public class PlayerMovement : MonoBehaviour
         _playerDirH = (int)inputDirFloat.x;
         _playerDirV = (int)inputDirFloat.y;
         //Debug.Log(inputDirFloat);
+
+        if (_anim.GetInteger("PlayerDirH") != _playerDirH)
+            _anim.SetInteger("PlayerDirH", _playerDirH);
+        if (_anim.GetInteger("PlayerDirV") != _playerDirV)
+            _anim.SetInteger("PlayerDirV", _playerDirV);
+        _anim.speed = _animSpeed;
 
         transform.position += _moveSpeed * Time.fixedDeltaTime * new Vector3(_playerDirH, _playerDirV, 0);
     }
