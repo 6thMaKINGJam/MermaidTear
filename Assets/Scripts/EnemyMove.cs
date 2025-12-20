c605906537b839b95cff3ad80b4b3e04fe18f743
nebobyeoli <nebobyeoli@gmail.com>
2025-12-20 14:11:11 +0900

    적 왕복 움직임 구현

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 3;
+
+    [SerializeField] private Transform _positionA;
+    [SerializeField] private Transform _positionB;
+    private bool currTarget;
+    private Vector3 getCurrTarget() => currTarget ? _positionB.position : _positionA.position;
+
+    private void FixedUpdate()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, getCurrTarget(), _moveSpeed * Time.fixedDeltaTime);
+
+        if (Vector3.Distance(transform.position, getCurrTarget()) < Mathf.Epsilon * 3)
+            currTarget = !currTarget; // swap current target
+    }
+}
